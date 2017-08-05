8ff5d76be0f13a9907c87b46ecc7beec5f24a81e
Enrique Genoves <e.gen.pur@gmail.com>
2017-08-05 22:01:12 +0200

    commit hello

---
+++ hello.asm
@@ -0,0 +1,22 @@
+global start
+
+
+section .text
+
+start:
+
+    mov     rax, 0x2000004 	; write call
+    mov     rdi, 1 		; stdout
+    mov     rsi, msg
+    mov     rdx, msg.len
+    syscall
+
+    mov     rax, 0x2000001 	; exit call
+    mov     rdi, 0
+    syscall
+
+
+section .data
+
+msg:    db      "Hello, world!", 10
+.len:   equ     $ - msg
